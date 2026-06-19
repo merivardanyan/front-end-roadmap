67a0c37641dbd1528baefe07bb00bec2dfdcfd1a
Meri Vardanyan <ms.merivardanyan@gmail.com>
2026-06-19 16:12:57 +0400

    Set up progress tracking with localStorage

--- js/progress.js
+++ js/progress.js
@@ -1,70 +1,90 @@
-const progress = JSON.parse(window.localStorage.getItem('progress')) || {};
 
+let progress = {};
+try {
+    progress = JSON.parse(localStorage.getItem('progress')) || {};
+} catch {
+    progress = {};
+}
 function loadProgress(chapters) {
-    progress.progress = 0;
-    progress.chapters = {};
-    Object.entries(chapters).forEach(([key, value]) => {
-        console.log(key, value.lessons);
-        const lessons = (value?.lessons || []).reduce((obj, e) => (
-            (value?.lessons || []).forEach(e => {
-                lessons[e.id] = {
+    if (!Object.values(progress).length) {
+        progress.progress = 0;
+        progress.totalChapters = Object.keys(chapters).length;
+        progress.chapters = {};
+        Object.entries(chapters).forEach(([key, value]) => {
+            const lessons = (value?.lessons || []).reduce((obj, e) => {
+                // (value?.lessons || []).forEach(e => {
+                obj[e.id] = {
                     completedQuestions: [],
                     progress: 0,
                     totalQuestions: 0
                 };
-            })
-        ), {});
-        progress.chapters[key] = {
-            chapter: key,
-            lessons,
-            progress: 0,
-            pending: 0
-        };
+                return obj;
+                // })
+            }, {});
+            progress.chapters[key] = {
+                chapter: key,
+                lessons,
+                progress: 0,
+                totalLessons: value?.lessons.length || 0
+            };
 
-    });
-    console.log(progress);
-    localStorage.setItem('progress', JSON.stringify(progress));
+        });
+        localStorage.setItem('progress', JSON.stringify(progress));
+    }
 }
 
-function loadChapterProgress(chapter) {
-    progress.chapters[chapter].pending = Object.values(progress.chapters[chapter].lessons).filter(e => e.progress === 0).length
-}
 
 function saveProgress() {
     localStorage.setItem('progress', JSON.stringify(progress));
 }
 function updateLessonProgress(lesson, question) {
+   
+    if(lesson.completedQuestions?.includes(question))
+        return false;
 
-    if (lesson.completedQuestions.indexOf(question) > -1) return false;
-    lesson.completedQuestions.push(question);
-    lesson.progress = lesson.completedQuestions.length
-    return true
+    lesson.completedQuestions.push(question); 
+
+    
+    lesson.progress =
+        (lesson.completedQuestions.length /
+        lesson.totalQuestions) * 100;
+
+    return true;
 }
-function updateChapterProgress(chapter, lesson) {
-    const chapterProgress = Object.values(progress.chapters[chapter]?.lessons).reduce((obj, e) => {
-        obj.progress += e.progress;
-        obj.totalQuestions += e.totalQuestions;
-        return obj
-    }, { progress: 0, totalQuestions: 0 });
-    progress.chapters[chapter].progress = chapterProgress.progress / chapterProgress.totalQuestions * 100;
-    saveProgress()
+function updateChapterProgress(chapter) {
+    if (!chapter.totalLessons) {
+        chapter.progress = 0;
+        return;
+    }
+    const chapterLessons = Object.values(chapter?.lessons) || [];
+    chapter.progress = (chapterLessons.reduce((s, e) => s + e.progress, 0) / chapter.totalLessons);
 }
 function updateOverallProgress() {
-    const overallProgress = Object.values(progress.chapters).reduce((obj, e) => {
-        obj.progress += e.progress;
-        obj.pending += e.pending;
-        return obj
-    }, { progress: 0, pending: 0 });
-    progress.progress = overallProgress.progress / overallProgress.pending;
+    if (!progress.totalChapters) {
+        progress.progress = 0;
+        return;
+    }
+    const chapters = (Object.values(progress?.chapters)) || [];
+    progress.progress = ((chapters.reduce((s, e) => s + e.progress, 0)) / (progress.totalChapters * 100));
+}
+
+function completeQuestion(questionCount = 0, chapter = 'core-web-foundations', lesson = 'how-the-web-works', questionID = "q3") {
+
+    const chapterInStorage = progress.chapters?.[chapter];
+    const lessonInStorage = chapterInStorage?.lessons?.[lesson];
+    if (!lessonInStorage) return;
+
+   
+    if (!updateLessonProgress(lessonInStorage, questionID)) return;
+    updateChapterProgress(chapterInStorage)
+    updateOverallProgress()
     saveProgress()
 }
 
-function completeQuestion(chapter, lesson, question){
-    const chapterPercentage = 100 / Object.values(chapters).length;
-
-    updateLessonProgress(chapter, lesson, question);
-    updateChapterProgress(chapter, lesson);
-    updateOverallProgress();
+function initializeLesson(chapterSlug,lessonSlug, lessonData){
+    const lessonInStorage = progress.chapters?.[chapterSlug]?.lessons?.[lessonSlug];
+     if (!lessonInStorage?.totalQuestions && lessonData.questions?.length > 0) {
+        lessonInStorage.totalQuestions = lessonData.questions.length
+    }
+    saveProgress()
 }
-
-// the progress saving logic is not completed
